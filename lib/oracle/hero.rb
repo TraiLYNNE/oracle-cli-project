f8adcc78c7a95909181f4d42374e71b30413d882
Trai Compton <tlcompton2546@outlook.com>
2017-03-25 22:40:56 +0000

    fixed initialize to accept hash and added fake data to controller to test cl and hero class.  tested.

--- lib/oracle/hero.rb
+++ lib/oracle/hero.rb
@@ -1,15 +1,14 @@
 class Hero
   #attributes and variables
-  attr_accessor :real_name, :current_alias, :affiliation, :boo, :identity, :citizenship, :marital_status, :occupation, :race, :gender, :height, :weight, :eyes, :hair, :skin, :universe, :pob
+  attr_accessor :real_name, :current_alias, :affiliation, :boo, :identity, :citizenship, :marital_status, :occupation, :gender, :eyes, :hair, :universe, :pob
   @@all = Array.new
 
   #modules
 
 
   #initialize
-  def initialize (real_name = "Bruce Wayne", current_alias = "Batman")
-    @real_name = real_name
-    @current_alias = current_alias
+  def initialize (hero_details)
+    hero_details.each{|k,v| self.send(("#{k}="), v)}
     self.class.all << self
   end
 
@@ -34,15 +33,11 @@ class Hero
     puts "Citizenship: #{citizenship}"
     puts "Marital Status: #{marital_status}"
     puts "Occupation: #{occupation}"
-    puts "Race: #{race}"
     puts "--------------"
     puts "Characteristices"
     puts "Gender: #{gender}"
-    puts "Height: #{height}"
-    puts "Weight: #{weight}"
     puts "Eyes: #{eyes}"
     puts "Hair: #{hair}"
-    puts "Skin: #{skin}"
     puts "--------------"
     puts "Origin:"
     puts "Universe: #{universe}"
