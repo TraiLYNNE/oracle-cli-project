f7471e1caae409ebd1d0743509156f27dcc22d2b
Trai Compton <tlcompton2546@outlook.com>
2017-03-25 22:00:14 +0000

    encapusulated #select_option, created loop effect. tested.

--- lib/oracle/cli.rb
+++ lib/oracle/cli.rb
@@ -3,17 +3,18 @@ class Oracle::CLI
     puts "Welcome."
     password
     list_options
-    puts "Who would you like more information about?"
-    puts "(Select a number, say 'List', or 'done')"
-    input = gets.chomp
-    case input
-    when "list"
-      list_options
-    when "done"
-      puts "Right then. Back to work"
-    else
-      puts "details"
-    end
+    # puts "Who would you like more information about?"
+    # puts "(Select a number, say 'List', or 'done')"
+    # input = gets.chomp
+    # case input
+    # when "list"
+    #   list_options
+    # when "done"
+    #   puts "Right then. Back to work"
+    # else
+    #   puts "details"
+    # end
+    select_option
   end
 
   def password
@@ -28,5 +29,21 @@ class Oracle::CLI
   def list_options
     puts "Here are your allies:"
     Hero.all.each.with_index {|a,i| puts "#{i+1}. #{a}"}
+  end
+
+  def select_option
+    puts "Who would you like more information about?"
+    puts "(Select a number, say 'list', or 'done')"
+    input = gets.chomp
+    case input
+    when "list"
+      list_options
+      select_option
+    when "done"
+      puts "Right then. Back to work"
+    else
+      puts "details"
+      select_option
+    end
   end
 end
