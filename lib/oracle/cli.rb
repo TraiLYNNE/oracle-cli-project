0277dafb9a3683c61e72b78aabbe3370f2eb9415
Trai Compton <tlcompton2546@outlook.com>
2017-03-25 21:36:50 +0000

    encapsulated #LIST_OPTIONS method. Tested.

--- lib/oracle/cli.rb
+++ lib/oracle/cli.rb
@@ -2,19 +2,23 @@ class Oracle::CLI
   def call
     puts "Welcome."
     password
-    puts "Here are you allies:"
-    allies = ["1", "2", "3", "4"]
-    allies.each.with_index {|a, i| puts "#{i+1}. #{a}"}
+    # puts "Here are you allies:"
+    # allies = ["1", "2", "3", "4"]
+    # allies.each.with_index {|a, i| puts "#{i+1}. #{a}"}
+    list_options
   end
 
   def password
     puts "Please enter your secret identity:"
     pw_input = STDIN.noecho(&:gets).chomp
-    if pw_input == "Bruce Wayne"
-      puts "options"
-    else
+    if pw_input != "Bruce Wayne"
       puts "PasswordError" #Define later
       password
     end
+  end
+
+  def list_options(hero_array = ["1", "2", "3", "4"])
+    puts "Here are your allies:"
+    hero_array.each.with_index {|a,i| puts "#{i+1}. #{a}"}
   end
 end
