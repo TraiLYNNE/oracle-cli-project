3c31559f47980a320f0c6e435e92b45debd55337
Trai Compton <tlcompton2546@outlook.com>
2017-03-25 21:44:48 +0000

    defined hero class, added @@all and it's reader

---
+++ lib/oracle/hero.rb
@@ -0,0 +1,18 @@
+class Hero
+  #attributes and variables
+  @@all = ["Batman", "Aquaman", "Wonder Woman", "Cyborg"]
+
+  #modules
+
+
+  #initialize
+
+
+  #class methods
+  def self.all
+    @@all
+  end
+
+
+  #instance methods
+end
