98e04d0b2c8be10b2a6f0e08adb643488431ee28
Trai Compton <tlcompton2546@outlook.com>
2017-03-25 21:22:22 +0000

    encapsulated password logic in method and called in #CALL. Tested.

--- lib/oracle/cli.rb
+++ lib/oracle/cli.rb
@@ -1,13 +1,17 @@
 class Oracle::CLI
   def call
     puts "Welcome."
+    password
+  end
+
+  def password
     puts "Please enter your secret identity:"
     pw_input = STDIN.noecho(&:gets).chomp
     if pw_input == "Bruce Wayne"
       puts "options"
     else
-      puts "PasswordError"
-      call
+      puts "PasswordError" #Define later
+      password
     end
   end
 end
